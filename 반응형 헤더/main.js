915fe14340546feb5c6e2b86ede2b43c9d779802
WHOIYA <120172398+WHOIYA@users.noreply.github.com>
2023-03-12 17:15:57 +0900

    Add files via upload

---
+++ 반응형 헤더/main.js
@@ -0,0 +1,26 @@
+const tooglenv = document.querySelector('.naver__toogle');
+const body = document.querySelector('.naver__toogle');
+const icons = document.querySelector('.naver__toogle');
+
+toogle.addEventListener('click', () => {
+    body.classList.toogle('active')
+    icons.classList.toogle('active')
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
